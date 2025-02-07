acd7d9ef3366551e2f108f9abc69f97a39aab0fb
xavier <hello@xavier.engineering>
2025-02-07 14:20:25 -0500

    add signup page

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,37 @@
+import { GalleryVerticalEnd } from "lucide-react"
+import Image from "next/image";
+
+import { SignUpForm } from "@/components/signup-form"
+
+export default function SignUpPage() {
+  return (
+    <div className="grid min-h-svh lg:grid-cols-2">
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2">
+          <a href="#" className="flex items-center gap-2 font-medium">
+            <Image
+                className="dak:invert"
+                src="/snapture_logo.svg"
+                alt="Next.js logo"
+                width={124}
+                height={38}
+                priority
+            />
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <SignUpForm />
+          </div>
+        </div>
+      </div>
+      <div className="relative hidden bg-muted lg:block">
+        <img
+          src="/login_hero.png"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useRouter } from "next/navigation";
 
-export function LoginForm({
+export function SignUpForm({
   className,
   ...props
 }: React.ComponentPropsWithoutRef<"form">) {
@@ -26,9 +26,9 @@ export function LoginForm({
       {...props}
     >
       <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to Account</h1>
+        <h1 className="text-2xl font-bold">Try Snapture for Free</h1>
         <p className="text-balance text-sm text-muted-foreground">
-          Enter credentials below to login to your account
+          Signup below to get started w/ Snapture {"\u00A0"}🚀
         </p>
       </div>
       <div className="grid gap-6">
@@ -40,24 +40,25 @@ export function LoginForm({
         <div className="grid gap-2">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a
-              href="#"
-              className="ml-auto text-sm underline-offset-4 hover:underline"
-            >
-              Forgot your password?
-            </a>
+          </div>
+          {/* TODO: Add `required` to the input below */}
+          <Input id="password" type="password"/>
+        </div>
+        <div className="grid gap-2">
+          <div className="flex items-center">
+            <Label htmlFor="password">Confirm Password</Label>
           </div>
           {/* TODO: Add `required` to the input below */}
           <Input id="password" type="password"/>
         </div>
         <Button type="submit" className="w-full">
-          Login
+         Get Snapture
         </Button>
       </div>
       <div className="text-center text-sm">
         👋 {"\u00A0"} {"\u00A0"}
-        <a href="/signup" className="underline underline-offset-4">
-          I don´t have a Snapture account{" "}
+        <a href="/login" className="underline underline-offset-4">
+            I already have a Snapture account{" "}
         </a>
       </div>
     </form>
