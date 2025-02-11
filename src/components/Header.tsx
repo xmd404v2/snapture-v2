f8dd5316013bcaef50161ceb355da78e0ff7d96c
jackht7 <jackhong.tey@gmail.com>
2025-02-11 13:42:36 +0800

    light theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,39 +1,20 @@
 // components/Header.tsx
-"use client";
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
 
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger,
-} from "@/components/ui/menubar"
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger } from '@/components/ui/menubar';
 
-import { 
-  Avatar, 
-  AvatarFallback, 
-  AvatarImage 
-} from "@/components/ui/avatar"
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
-import Link from "next/link";
-import { MenuBar } from "@/components/Menu";
+import Link from 'next/link';
+import { MenuBar } from '@/components/menu';
 
 export function Header() {
   return (
-    <header className="bg-background border-b border-border">
-      <div className="container mx-auto flex items-center justify-between px-4">
-        <Link href="/dashboard" className="text-2xl font-bold">
-          <Image
-            className="dak:invert"
-            src="/snapture_logo.svg"
-            alt="Next.js logo"
-            width={104}
-            height={32}
-            priority
-          />
+    <header className='bg-background border-b border-border'>
+      <div className='container mx-auto flex items-center justify-between px-4'>
+        <Link href='/dashboard' className='text-2xl font-bold'>
+          <Image src='/snapture_black.svg' alt='Next.js logo' width={104} height={32} priority />
         </Link>
         <MenuBar />
       </div>
